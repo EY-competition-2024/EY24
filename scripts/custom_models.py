4f12e1e229aee3f939817c3c01c9ea6cad392c9a
Queeno11 <abbatenicolas@gmail.com>
2024-03-03 12:19:35 -0300

    Correccion de bug
    error en la definicion de la bounding_box_format. Tambien paso el modelo a uno mas chico

--- scripts/custom_models.py
+++ scripts/custom_models.py
@@ -54,9 +54,9 @@ def YOLOv8(n_classes=2) -> Sequential:
     # )
     model = keras_cv.models.YOLOV8Detector(
         num_classes=n_classes,
-        bounding_box_format="xywh",
-        backbone=keras_cv.models.YOLOV8Backbone.from_preset("yolo_v8_m_backbone_coco"),
-        fpn_depth=2,
+        bounding_box_format="xyxy",
+        backbone=keras_cv.models.YOLOV8Backbone.from_preset("yolo_v8_s_backbone_coco"),
+        fpn_depth=1,
     )
 
     return model
